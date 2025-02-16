41450ca27e8525b68d1a227fa4c5bd2a750bd1b4
kaii55 <arit.pom95@gmail.com>
2025-02-16 12:51:09 +0000

    Added Buildup and Direct Attacks metrics and integrated into pipeline

--- src/feature_engineering/metrics.py
+++ src/feature_engineering/metrics.py
@@ -6,6 +6,9 @@ from maintain_buildup_sustain import calculate_maintain_buildup_sustain
 from speed_metrics import calculate_speed_metrics
 from passes_per_sequence import calculate_avg_passes_per_sequence
 from attacking_passes_per_sequence import calculate_avg_attacking_passes_per_sequence
+from verticality import calculate_avg_verticality
+from defensive_height import calculate_avg_defensive_height
+from attacks import calculate_buildup_and_direct_attacks
 
 # File paths
 input_parquet = "/Users/aritramajumdar/Desktop/Football_project/data/processed/J_League_data.parquet"
@@ -22,6 +25,9 @@ mbs = calculate_maintain_buildup_sustain(df)
 speed_metrics = calculate_speed_metrics(df)
 avg_passes = calculate_avg_passes_per_sequence(df)
 avg_attacking_passes = calculate_avg_attacking_passes_per_sequence(df)
+avg_verticality = calculate_avg_verticality(df)
+avg_defensive_height = calculate_avg_defensive_height(df)
+attacks = calculate_buildup_and_direct_attacks(df)
 
 # Merge all metrics into a single DataFrame
 metrics_df = (
@@ -32,6 +38,9 @@ metrics_df = (
     .merge(speed_metrics, on='Team', how='left')
     .merge(avg_passes, on='Team', how='left')
     .merge(avg_attacking_passes, on='Team', how='left')
+    .merge(avg_verticality, on='Team', how='left')
+    .merge(avg_defensive_height, on='Team', how='left')
+    .merge(attacks, on='Team', how='left')
 )
 
 # Save the metrics to an Excel file
